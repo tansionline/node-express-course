d9a26f3959b298e5007c1faed397ed9cdb483f9f
tansionline <2014513023@ogr.mehmetakif.edu.tr>
2020-12-05 11:54:48 +0300

    add login POST route

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.json())
 
 const mockUserData = [
     {name: 'Mark'},
     {name: 'Jill'}
 ]
 
-// app.get('/users', function(req, res){
-//     res.json({
-//         success: true,
-//         message: 'successfully got users. Nice!',
-//         users: mockUserData
-//     })
-// })
+app.get('/users', function(req, res){
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    })
+})
 
 app.get('/users/:id', function(req, res){
     res.json({
@@ -21,6 +24,30 @@ app.get('/users/:id', function(req, res){
         users: req.params.id
     })
 })
+
+
+app.post('/login', function(req, res) {
+    const username = req.body.username
+    const password = req.body.password
+
+    const mockUsername = 'billyTheKid'
+    const mockPassword = 'superSecret'
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    }
+    else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+})
+
 
 app.listen(8000, function(){
     console.log('Server is running')
